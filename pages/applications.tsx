750d722dbf7a477035c6111e47a81dcac2f953f7
Arnold Yee <113506481+arnoldyee3@users.noreply.github.com>
2024-11-22 06:59:12 -0500

    Just the JSX changes

---
+++ pages/applications.tsx
@@ -0,0 +1,11 @@
+import { Group } from "@mantine/core";
+
+export default function ApplicationsPage() {
+    return (
+      <div>
+        <Group mt={50} justify="center">
+          <h1>Applications Page</h1>
+        </Group>
+      </div>
+    );
+}
---
+++ pages/stats.tsx
@@ -0,0 +1,11 @@
+import { Group } from "@mantine/core";
+
+export default function statsPage() {
+    return (
+      <div>
+        <Group mt={50} justify="center">
+          <h1>Stats Page</h1>
+        </Group>
+      </div>
+    );
+}
