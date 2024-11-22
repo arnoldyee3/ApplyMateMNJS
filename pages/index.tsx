9a7eca1f5f0088c57c4367316e79341632e08327
Arnold Yee <113506481+arnoldyee3@users.noreply.github.com>
2024-11-22 06:38:55 -0500

    Initial Setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,19 @@
 import { Button, Group } from "@mantine/core";
+import { useRouter } from 'next/router'
 
 export default function IndexPage() {
+  const router = useRouter()
+
   return (
-    <Group mt={50} justify="center">
-      <Button size="xl">Welcome to Mantine!</Button>
-    </Group>
+    <div>
+      <Group mt={50} justify="center">
+        <h1>ApplyMate</h1>
+      </Group>
+      <Group mt={50} justify="center">
+        <Button size="xl" onClick={() => router.push('/applications')}>Applications</Button>
+        <Button size="xl" onClick={() => router.push('/journaling')}>Journaling</Button>
+        <Button size="xl" onClick={() => router.push('/stats')}>Stats </Button>
+      </Group>
+    </div>
   );
 }
